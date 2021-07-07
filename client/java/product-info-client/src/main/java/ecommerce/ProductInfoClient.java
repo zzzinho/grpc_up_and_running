c287df4cb7f7999e97e2d5966636466a5bbab551
jinho <hahoh0013@gmail.com>
2021-07-07 14:14:15 +0900

    go and java, server and client

--- client/java/product-info-client/src/main/java/ecommerce/ProductInfoClient.java
+++ client/java/product-info-client/src/main/java/ecommerce/ProductInfoClient.java
@@ -7,8 +7,10 @@ import java.util.logging.Logger;
 
 public class ProductInfoClient {
     private static final Logger logger = Logger.getLogger(ProductInfoClient.class.getName());
+
     public static void main(String[] args) throws InterruptedException {
-        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50051)
+        ManagedChannel channel = ManagedChannelBuilder
+                .forAddress("localhost", 50051)
                 .usePlaintext()
                 .build();
 
@@ -18,7 +20,8 @@ public class ProductInfoClient {
         ProductInfoOuterClass.ProductID productID = stub.addProduct(
                 ProductInfoOuterClass.Product.newBuilder()
                         .setName("Samsung S10")
-                        .setDescription("Samsung Galaxy S10 is the latest smart phone, launched in February 2019")
+                        .setDescription("Samsung Galaxy S10 is the latest smart phone, " +
+                                "launched in February 2019")
                         .setPrice(700.0f)
                         .build());
         logger.info("Product ID: " + productID.getValue() + " added successfully.");
