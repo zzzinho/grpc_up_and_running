3c3d9e73f1fcfd602eeeb09bdeff958163b1bcdc
jinho <hahoh0013@gmail.com>
2021-07-11 16:24:59 +0900

    go :: order-service

---
+++ client/go/order-service/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"google.golang.org/grpc"
+
+	pb "order-service/ecommerce"
+)
+
+const (
+	address = "localhost:50051"
+)
+
+func main() {
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	client := pb.NewOrderManagementClient(conn)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+
+	order1 := pb.Order{Id: "101", Items: []string{"iPhone XS", "Mac Book Pro"}, Destination: "San Jose, CA", Price: 2300.00}
+	res, _ := client.AddOrder(ctx, &order1)
+	if res != nil {
+		log.Print("AddOrder Response -> ", res.Value)
+	}
+}
--- server/go/productinfo/service/go.mod
+++ server/go/productinfo/service/go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/gofrs/uuid v4.0.0+incompatible
-	google.golang.org/grpc v1.38.0
+	github.com/golang/protobuf v1.5.0
+	google.golang.org/grpc v1.39.0
 	google.golang.org/protobuf v1.26.0
 )
